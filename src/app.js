4c0d7b2e1dc98835e1d08ff043898f18c4c7792c
程晋瑞 <chengjinrui@babyfs.cn>
2020-06-09 00:42:02 +0800

    更新配置

--- src/app.js
+++ src/app.js
@@ -14,16 +14,12 @@ App({
   /**
    * 当小程序从前台进入后台，会触发 onHide
    */
-  onHide() {
-
-  },
+  onHide() {},
 
   /**
    * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
    */
-  onError(msg) {
-
-  },
+  onError(msg) {},
 
   globalData: {
     userInfo: {},
